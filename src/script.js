60fff212ebfed37be86f8529b07ff9a94217f9fc
Oleksii Liubynskyi <alex108@gmail.com>
2024-05-27 12:15:41 +0300

    sy-1 First commit

---
+++ src/script.js
@@ -0,0 +1,24 @@
+window.onerror=null;
+  
+
+function mlr(prop1, prop2) {
+	
+	//$('.mlc').each(function (i) {
+	//	$(this).html('<a href="mailto:' + $(this).attr('mln') + '@' +  $(this).attr('mls') + '">' + $(this).attr('mln') + ' @ ' + $(this).attr('mls') + '</a>');
+				  
+	//});
+	
+document.write('<a href="mailto:' + prop1 + '@' +  prop2 + '">' + prop1 + ' @ ' + prop2 + '</a><br>');	
+
+}
+
+
+
+function responsiveTable(tblObj,colNum){
+
+}	
+
+
+function showTextDiv(name) {
+
+}
